de8e7498f9503068c080008a3e98d6adde20e87a
TheMineMan100 <hout0058@hz.nl>
2023-02-11 18:12:35 +0100

    Done up to ex 1.33
    Made posts appear based on database post data

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -7,29 +7,29 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-    public function show($post)
+    public function show($slug)
     {
-//        $post = \DB::table('posts')->where('slug', $slug)->first();
-//
-//        if (! $post) {
-//            abort(404);
-//        }
-//
-//        return view('post', [
-//            'post' => $post
-//        ])
+        $post = \DB::table('posts')->where('slug', $slug)->first();
 
-        $posts = [
-            'my-first-post' => 'Hello, this is my first blog post!',
-            'my-second-post' => 'Now I am getting the hang of this blogging thing.'
-        ];
-
-        if (!array_key_exists($post, $posts)) {
-            abort(404, 'Sorry, that post was not found.');
+        if (! $post) {
+            abort(404);
         }
 
         return view('post', [
-            'post' => $posts[$post]
+            'post' => $post
         ]);
+
+//        $posts = [
+//            'my-first-post' => 'Hello, this is my first blog post!',
+//            'my-second-post' => 'Now I am getting the hang of this blogging thing.'
+//        ];
+//
+//        if (!array_key_exists($post, $posts)) {
+//            abort(404, 'Sorry, that post was not found.');
+//        }
+//
+//        return view('post', [
+//            'post' => $posts[$post]
+//        ]);
     }
 }
